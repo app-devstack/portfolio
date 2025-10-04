e910683d7334d75fb95b4949403c7d83289e06cd
rikiTada <riki.tada777@gmail.com>
2025-10-05 02:42:57 +0900

    update: Root layout style.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+import './globals.css';
+
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+
+import { cn } from '@/lib/utils';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -23,8 +26,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html lang="ja">
+      <body
+        className={cn(
+          geistSans.variable,
+          geistMono.variable,
+          'h-screen w-screen overflow-hidden antialiased'
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
