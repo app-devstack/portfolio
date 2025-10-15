81793d39d514423ab38889d6cbe9fdb966ac1272
rikiTada <riki.tada777@gmail.com>
2025-10-15 22:43:24 +0900

    feat: 簡易的なアニメーションを追加

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -6,7 +6,7 @@ import CustomCanvas from './components/three/customCanvas';
 export default function HomePage() {
   return (
     <div className="main relative h-screen w-screen">
-      <div className="absolute top-1/2 left-1/2 z-10 w-screen -translate-x-1/2 -translate-y-1/2 overflow-hidden">
+      <div className="absolute top-1/2 left-1/2 z-10 w-screen -translate-x-1/2 -translate-y-1/2">
         <MarqueeTitle />
       </div>
 
--- src/features/home/components/MarqueeTitle.tsx
+++ src/features/home/components/MarqueeTitle.tsx
@@ -11,7 +11,7 @@ export default function MarqueeTitle() {
   // 定数の定義
   const TITLE_WIDTH = isMobile ? 200 : 400;
   const TITLE_HEIGHT = isMobile ? 50 : 100;
-  const TITLE_GAP = isMobile ? 200 : 400;
+  const TITLE_GAP = isMobile ? 200 : 200;
   const TITLE_COUNT = 4;
   const ANIMATION_DURATION = 40;
 
@@ -21,11 +21,9 @@ export default function MarqueeTitle() {
 
   return (
     <motion.div
-      className="flex"
+      className="relative flex"
       style={{ gap: TITLE_GAP }}
-      animate={{
-        x: [0, MOVE_DISTANCE],
-      }}
+      animate={{ x: [0, MOVE_DISTANCE] }}
       transition={{
         x: {
           repeat: Infinity,
@@ -36,7 +34,34 @@ export default function MarqueeTitle() {
       }}
     >
       {Array.from({ length: TITLE_COUNT * 2 }).map((_, index) => (
-        <HeroTitle key={index} width={TITLE_WIDTH} height={TITLE_HEIGHT} />
+        <motion.div
+          key={index}
+          drag
+          dragTransition={{ bounceStiffness: 500, bounceDamping: 15 }}
+          dragConstraints={{ top: 0, right: 0, bottom: 0, left: 0 }}
+          whileHover={{
+            scale: 1.2,
+            rotate: 3,
+            transition: { duration: 0.2, type: 'spring', stiffness: 200 },
+          }}
+          whileTap={{
+            scale: 0.9,
+          }}
+          initial={{ y: -800, opacity: 0, scale: 0.1 }}
+          animate={{ y: 0, opacity: 1, scale: 1 }}
+          transition={{
+            delay: index * 0.5, // 順番に登場
+            type: 'spring',
+            bounce: 0.4,
+            duration: 0.8,
+
+            // ホバー解除時の戻りアニメーション
+            scale: { duration: 0.2, ease: 'easeInOut' },
+            rotate: { duration: 0.2, ease: 'easeInOut' },
+          }}
+        >
+          <HeroTitle width={TITLE_WIDTH} height={TITLE_HEIGHT} />
+        </motion.div>
       ))}
     </motion.div>
   );
