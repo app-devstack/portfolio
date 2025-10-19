054356c6fc92b4440230836477d36166483284be
rikiTada <riki.tada777@gmail.com>
2025-10-19 17:45:27 +0900

    delete: sizeの大きいファイルをR2に移動

---
+++ src/constants/index.ts
@@ -0,0 +1,6 @@
+/**
+ * アセット関連ドメイン
+ */
+export const ASSETS_DOMAIN = process.env.ASSETS_DOMAIN_NAME
+  ? `https://${process.env.ASSETS_DOMAIN_NAME}`
+  : `https://assets.maru-maruuu.com`;
--- src/features/home/components/three/figure.tsx
+++ src/features/home/components/three/figure.tsx
@@ -2,12 +2,15 @@
 import { useGLTF } from '@react-three/drei';
 import React from 'react';
 
+import { ASSETS_DOMAIN } from '@/constants';
+
 interface FigureProps {
   position?: [number, number, number];
 }
 
 export default function Figure({ position = [0, 0, 0] }: FigureProps) {
-  const gltf = useGLTF('/models/ice_cream_ghost.glb');
+  const path = `${ASSETS_DOMAIN}/assets/models/ice_cream_ghost.glb`;
+  const gltf = useGLTF(path);
 
   return (
     <group position={position}>
