a2c8492a8e5b5d15481ba0cbf0e133aa7f2a4640
rikiTada <riki.tada777@gmail.com>
2025-10-23 21:23:54 +0900

    move: フォント関連を一つのファイルから配信

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,9 @@
 import './globals.css';
 
 import type { Metadata } from 'next';
-import { Montserrat_Alternates, Noto_Sans_JP } from 'next/font/google';
 
+import { montserratAlternates, notoSansJp } from '@/assets/font';
 import { cn } from '@/lib/utils';
-
-// Noto Sans JP
-const notoSansJp = Noto_Sans_JP({
-  variable: '--font-noto-sans-jp',
-  subsets: ['latin'],
-  weight: ['100', '300', '400', '500', '600', '700', '900'],
-});
-
-// Montserrat Alternates
-const montserratAlternates = Montserrat_Alternates({
-  variable: '--font-montserrat-alternates',
-  subsets: ['latin'],
-  weight: ['100', '300', '400', '500', '600', '700', '900'],
-});
 
 export const metadata: Metadata = {
   title: 'maru-maruuu_portfolio',
