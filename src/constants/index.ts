422d095a9d94f80d62c612d628020614cf248559
rikiTada <riki.tada777@gmail.com>
2025-10-27 00:29:38 +0900

    feat: モデル読み込みパスを定数で管理

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,23 @@
 /**
+ * 本番環境かどうか
+ */
+export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
+
+/**
  * アセット関連ドメイン
  */
 export const ASSETS_DOMAIN = process.env.ASSETS_DOMAIN_NAME
   ? `https://${process.env.ASSETS_DOMAIN_NAME}`
   : `https://assets.maru-maruuu.com`;
+
+/**
+ * 3Dモデルのパス
+ */
+export const MODEL_PATHS = {
+  portfolio: IS_PRODUCTION
+    ? `${ASSETS_DOMAIN}/assets/models/portfolio.glb`
+    : '/models/portfolio.glb',
+  iceCreamGhost: IS_PRODUCTION
+    ? `${ASSETS_DOMAIN}/assets/models/ice_cream_ghost.glb`
+    : `/models/ice_cream_ghost.glb`,
+};
