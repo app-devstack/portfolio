de8bdf3b0684613bf691252427c25d09977b69f3
rikiTada <riki.tada777@gmail.com>
2025-10-30 22:21:41 +0900

    feat: フッターの調整
    * SNSリンクを反映
    * サイトロゴを追加

--- src/components/elements/footer/index.tsx
+++ src/components/elements/footer/index.tsx
@@ -1,56 +1,56 @@
 import { FileTextIcon, GithubIcon, TwitterIcon } from 'lucide-react';
+import Image from 'next/image';
 
 import { cn } from '@/lib/utils';
 
 export default function Footer() {
   const socialLinks = [
     {
-      name: 'X (Twitter)',
-      href: '/',
+      name: 'X(Twitter)',
+      href: 'https://x.com/ice_maru_maruuu',
       Icon: TwitterIcon,
-      label: 'X (Twitter)',
+      label: 'X(Twitter)',
     },
     {
       name: 'Note',
-      href: '/',
+      href: 'https://note.com/maru_maruuu',
       Icon: FileTextIcon,
       label: 'Note',
     },
     {
       name: 'GitHub',
-      href: '/',
+      href: 'https://github.com/maru-maruu',
       Icon: GithubIcon,
       label: 'GitHub',
     },
   ];
   return (
-    <div
-      className={cn(
-        'relative min-h-[300px] overflow-hidden'
-        // 'bg-background/10',
-        // 'border-forgrand border-t-[3px]'
-      )}
-    >
+    <div className={cn('relative min-h-[300px] overflow-hidden')}>
       <div className="relative z-10 flex flex-col items-center justify-between gap-8 p-8 md:p-12">
         <div className="flex w-full max-w-6xl items-start justify-between">
           {/* ロゴ */}
           <div className="flex flex-col gap-2">
-            <div className="h-12 w-12 rounded bg-gray-400" />
-            <p className="text-muted-foreground text-sm">maru-maruuu</p>
+            <div className="relative h-12 w-12">
+              <Image src={'/maruuu_logo.svg'} fill alt={'site logo'} />
+            </div>
+            <p className="text-foreground text-sm font-semibold">maru-maruuu</p>
           </div>
 
           {/* SNSアイコン */}
-          <div className="flex items-center gap-6">
+          <div className="flex items-center gap-2">
             {socialLinks.map(({ name, href, label, Icon }) => (
               <a
                 key={name}
                 href={href}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="text-muted-foreground/70 transition-opacity hover:opacity-70"
+                className={cn(
+                  'text-muted-foreground grid aspect-square min-w-16 place-items-center opacity-80 transition-opacity hover:opacity-70'
+                )}
                 aria-label={label}
               >
-                <Icon size={24} />
+                <Icon size={32} />
+                <span className="text-xs">{name}</span>
               </a>
             ))}
           </div>
