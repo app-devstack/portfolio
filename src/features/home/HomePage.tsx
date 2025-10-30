e3f92e28ded37aa1d2e7d6d553003438d9a85856
tada rikimaru <122006815+rikiTada@users.noreply.github.com>
2025-10-30 12:23:27 +0000

    feat: 感謝メッセージを追加

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -9,6 +9,7 @@ import { ScrollProvider } from '@/contexts/ScrollContext';
 import HomeMainContents from '@/features/home/components/contents';
 import MarqueeContents from '@/features/home/components/marquee';
 import HomeThreeCanvas from '@/features/home/three/homeThreeCanvas';
+import { cn } from '@/lib/utils';
 
 export default function HomePage() {
   const { scrollY } = useScroll();
@@ -36,6 +37,18 @@ export default function HomePage() {
         <div className="overflow-x-hidden overflow-y-visible">
           {/* メインコンテンツ */}
           <HomeMainContents />
+        </div>
+
+        <div className={cn('z-10 overflow-x-hidden overflow-y-visible')}>
+          <div
+            className={cn(
+              'h-[80vh] px-12 py-32',
+              'text-foreground text-xl/12 font-bold tracking-wide md:text-3xl/24'
+            )}
+          >
+            <p>Thankyou for visiting my portfolio site!</p>
+            <p>mite kurete arigato~!!</p>
+          </div>
         </div>
 
         {/* フッター */}
