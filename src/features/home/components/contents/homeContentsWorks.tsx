3ec700f37935ae20c6d54d9c08c48d9692a643ed
tada rikimaru <122006815+rikiTada@users.noreply.github.com>
2025-10-30 12:23:58 +0000

    WIP: もっと見るボタンの装飾を追加

--- src/features/home/components/contents/homeContentsWorks.tsx
+++ src/features/home/components/contents/homeContentsWorks.tsx
@@ -1,6 +1,6 @@
-import { FastForwardIcon } from 'lucide-react';
+import { ArrowRightIcon } from 'lucide-react';
 
-import { Button } from '@/components/ui/button';
+import { AnimatedShinyText } from '@/components/ui/animated-shiny-text';
 import { cn } from '@/lib/utils';
 
 export default function HomeContentsWorks() {
@@ -23,11 +23,32 @@ export default function HomeContentsWorks() {
       </ul>
 
       <div className="ml-auto">
-        <Button className="h-20 max-w-xs px-10 text-xl font-bold">
-          もっと見る
-          <FastForwardIcon size={32} strokeWidth={2.5} className="fill-white" />
-        </Button>
+        <AnimatedShinyButton />
       </div>
+    </div>
+  );
+}
+
+function AnimatedShinyButton() {
+  return (
+    <div className="z-10 flex min-h-64 items-center justify-center">
+      <button
+        className={cn(
+          'group rounded-full border border-black/5 bg-neutral-100 text-base text-white transition-all ease-in hover:cursor-pointer hover:bg-neutral-200 dark:border-white/5 dark:bg-neutral-900 dark:hover:bg-neutral-800'
+        )}
+      >
+        <AnimatedShinyText className="inline-flex items-center justify-center px-4 py-1 transition ease-out hover:text-neutral-500 hover:duration-300 hover:dark:text-neutral-400">
+          <span className="inline-flex h-20 max-w-xs items-center justify-center px-10 text-xl font-bold hover:cursor-pointer">
+            もっと見る
+            <ArrowRightIcon
+              size={32}
+              strokeWidth={2.5}
+              className="ml-1 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5"
+            />
+          </span>
+          {/* <span>✨ Introducing Magic UI</span> */}
+        </AnimatedShinyText>
+      </button>
     </div>
   );
 }
